2ba904e66cbb589e77af8317cdb003858af3368a
Alexander DeForge <alexanderdeforge@gmail.com>
2021-12-27 15:38:30 -0500

    Updated how caching works. Removed commented out code.

--- mining/miningHelper/mineClinicalTrials.js
+++ mining/miningHelper/mineClinicalTrials.js
@@ -6,6 +6,10 @@ const {
 } = require('../../common/utils');
 const apis = require('../../common/apis');
 const fs = require('fs');
+const {
+  loadCache,
+  writeToCache
+} = require('../miningHelper/miningCacher');
 
 
 // caching feature for pubmed clinical trials by publication
@@ -13,69 +17,25 @@ const fs = require('fs');
 //  projects are not, the number of clinical trials associated with a project may change over time
 let pubmed_clinicaltrial_cache_publications = {};
 const pubmed_clinicaltrial_cache_publications_location = "config/pubmed_clinicaltrial_publications_cache.tsv";
+const pubmed_clinicaltrial_cache_publications_columns = ["publication", "clinical_trial_id"];
 
-const loadPubMedClinicalTrialCache = () => {
-  let corrupt_lines = false;
-  let columns = ["publication", "clinical_trial_id"];
-  var data = null;
-  try {
-    data = fs.readFileSync(pubmed_clinicaltrial_cache_publications_location).toString();
-    data = data.split('\n');  // get each line
-    for (var i = 1; i < data.length; i++) {  // parse each line, skipping the header
-      if (data[i] === "") {  // check for empty lines
-        continue;
-      }
-      let line = data[i].split("\t");
-      if (line.length !== columns.length) {  // check line integrity
-        console.log("PubMed publication cache line corrupted, marking cache file for correction.");
-        corrupt_lines = true;
-        continue;  // move on to next line
-      }
-      for (var j = 1; j < line.length; j++) {  // parse columns, index starts at 1 to skip the pmid, which is hard-coded as a key
-        if (!pubmed_clinicaltrial_cache_publications[line[0]]) {
-          pubmed_clinicaltrial_cache_publications[line[0]] = [];  // the pubmed cache for clinical trials is a dictionary of pmid keys that point to an array of clinical trial id's
-        }
-        if (pubmed_clinicaltrial_cache_publications[line[0]].indexOf(line[j]) === -1) {
-          pubmed_clinicaltrial_cache_publications[line[0]].push(line[j]); // load the cache
-        }
-      }
+const formatCache = () => {
+  const keys = Object.keys(pubmed_clinicaltrial_cache_publications);
+  for (var i = 0; i < keys.length; i++) {
+    if (pubmed_clinicaltrial_cache_publications[keys[i]]["clinical_trial_id"] === "") {
+      pubmed_clinicaltrial_cache_publications[keys[i]]["clinical_trial_id"] = [""];
     }
-    if (corrupt_lines === true) {  // overwrite corrupted cache file with known-good read cache, if any corrupted lines
-      console.log("Rewriting cache file due to corrupted line(s).");
-      let new_data = columns.join("\t") + "\n";
-      const pubmed_keys = Object.keys(pubmed_clinicaltrial_cache_publications);
-      for (var i = 0; i < pubmed_keys.length; i++) {
-        pubmed_clinicaltrial_cache_publications[pubmed_keys[i]].forEach((clinicalTrial) => {
-          let tmp = [];
-          tmp.push(pubmed_keys[i]);
-          tmp.push(clinicalTrial);
-          new_data += tmp.join("\t") + "\n";
-        });
-      }
-      fs.writeFileSync(pubmed_clinicaltrial_cache_publications_location, new_data)
+    else {
+      pubmed_clinicaltrial_cache_publications[keys[i]]["clinical_trial_id"] = pubmed_clinicaltrial_cache_publications[keys[i]]["clinical_trial_id"].split(",");  // turn a comma delimited string into an array of values
     }
   }
-  catch (error) {  // check if the file exists
-    // console.log(error);
-    console.log("File doesn't exist, writing");
-    const header = columns.join("\t") + "\n";
-    fs.writeFileSync(pubmed_clinicaltrial_cache_publications_location, header);
-  }
-}
-
-const writeToPubMedClinicalTrialCache = (pmid, clinicalTrialID) => {
-  let data = "";
-  let tmp = [];
-  tmp.push(pmid);
-  tmp.push(clinicalTrialID);
-  data += tmp.join("\t") + "\n";
-  fs.appendFileSync(pubmed_clinicaltrial_cache_publications_location, data);
-}
+};
 
 // mine clinical trials by project and publication
 const run = async (projects, publications, clinicalTrials) => {
   console.log("Loading PubMed clinical trial cache.");
-  loadPubMedClinicalTrialCache();
+  pubmed_clinicaltrial_cache_publications = loadCache(pubmed_clinicaltrial_cache_publications_location, pubmed_clinicaltrial_cache_publications_columns);
+  formatCache();
   console.log("Number of cached publications for clinical trials loaded: " + Object.keys(pubmed_clinicaltrial_cache_publications).length);
 
   // mine clinical trials by project
@@ -146,12 +106,12 @@ const run = async (projects, publications, clinicalTrials) => {
     // check for a cache hit
     if (pubmed_clinicaltrial_cache_publications[publicationNums[i]]) {
       console.log("PubMed cache hit for publication: " + publicationNums[i]);
-      if (pubmed_clinicaltrial_cache_publications[publicationNums[i]].indexOf("") > -1) {  // the presence of an empty string means empty array (no clinical trials)
+      if (pubmed_clinicaltrial_cache_publications[publicationNums[i]]["clinical_trial_id"].indexOf("") > -1) {  // the presence of an empty string means empty array (no clinical trials)
         publications[publicationNums[i]].clinicalTrials = [];
       }
       else {
-        publications[publicationNums[i]].clinicalTrials = pubmed_clinicaltrial_cache_publications[publicationNums[i]];  // store clinical trials in publications data structure
-        pubmed_clinicaltrial_cache_publications[publicationNums[i]].forEach((ct) => {
+        publications[publicationNums[i]].clinicalTrials = pubmed_clinicaltrial_cache_publications[publicationNums[i]]["clinical_trial_id"];  // store clinical trials in publications data structure
+        pubmed_clinicaltrial_cache_publications[publicationNums[i]]["clinical_trial_id"].forEach((ct) => {
           if (!clinicalTrials[ct]) {
             clinicalTrials[ct] = {};
           }
@@ -178,7 +138,6 @@ const run = async (projects, publications, clinicalTrials) => {
         if(publications[publicationNums[i]].clinicalTrials.indexOf(clinical_trial) === -1){
           // associate clinical trial id only with publication
           publications[publicationNums[i]].clinicalTrials.push(clinical_trial);
-          writeToPubMedClinicalTrialCache(publicationNums[i], clinical_trial);  // cache a clinical trial that is new to a publication
         }
         // save clinical trial id in clinicalTrials data structure to be populated with details later
         if (!clinicalTrials[clinical_trial]) {
@@ -195,7 +154,10 @@ const run = async (projects, publications, clinicalTrials) => {
         idx_start = tmp.indexOf("id=\"supplemental-data-ClinicalTrials.gov-NCT");  // get the next clinical trial if it is there
       }
       if (!publications[publicationNums[i]].clinicalTrials) {
-        writeToPubMedClinicalTrialCache(publicationNums[i], "");  // if there were no clinical trials for a publication, we want to preserve that with the empty string
+        writeToCache(pubmed_clinicaltrial_cache_publications_location, [publicationNums[i], ""]);  // if there were no clinical trials for a publication, we want to preserve that with the empty string
+      }
+      else {
+        writeToCache(pubmed_clinicaltrial_cache_publications_location, [publicationNums[i], publications[publicationNums[i]].clinicalTrials.join(",")])  // save the clinical trials for this publication as a csv string in the clinical_trial_id column
       }
     }
     console.log(`Collected ${publications[publicationNums[i]].clinicalTrials ? publications[publicationNums[i]].clinicalTrials.length : 0} Clinical Trials for publication: ${publicationNums[i]} [publication ${i+1} of ${publicationNums.length}]\n`);
