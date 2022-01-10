56b2961d5093754111ed3e68e04c05139852302b
Alexander DeForge <alexanderdeforge@gmail.com>
2022-01-10 16:09:44 -0500

    Update how data structures are formatted and written to file -- toward ingestion into Neo4j database.

--- mining/dataMining.js
+++ mining/dataMining.js
@@ -7,6 +7,10 @@ const mineSRADetail = require('./miningHelper/mineSRADetail');
 const mineGEODetail = require('./miningHelper/mineGEODetail');
 const mineDBGapDetail = require('./miningHelper/mineDBGapDetail');
 const mineClinicalTrialsDetail = require('./miningHelper/mineClinicalTrialsDetail');
+const {
+  getCoreId,
+  getActivityCode
+} = require('../common/utils');
 
 
 let projects = {};
@@ -19,7 +23,7 @@ let clinicalTrials = {};
 let metrics = {};
 
 const generateDataModel = async () => {
-  // associate publications for GEO, SRA, dbGap
+  // associate publications and projects for GEO, SRA, dbGap
   for(let pid in publications){
     //GEO
     for (let g = 0; g < publications[pid].geo_accession.length; g++) {
@@ -29,6 +33,12 @@ const generateDataModel = async () => {
           geos[geo].publications = [];
         }
         geos[geo].publications.push(pid);
+        if (!geos[geo].projects) {
+          geos[geo].projects = [];
+        }
+        publications[pid].projects.forEach(project => {
+          geos[geo].projects.push(project);
+        });
       }
     }
     //SRA
@@ -39,6 +49,12 @@ const generateDataModel = async () => {
           sras[sra].publications = [];
         }
         sras[sra].publications.push(pid);
+        if (!sras[sra].projects) {
+          sras[sra].projects = [];
+        }
+        publications[pid].projects.forEach(project => {
+          sras[sra].projects.push(project);
+        });
       }
     }
     //dbGap
@@ -49,7 +65,28 @@ const generateDataModel = async () => {
           dbgaps[dbgap].publications = [];
         }
         dbgaps[dbgap].publications.push(pid);
+        if (!dbgaps[dbgap].projects) {
+          dbgaps[dbgap].projects = [];
+        }
+        publications[pid].projects.forEach(project => {
+          dbgaps[dbgap].projects.push(project);
+        });
       }
+    }
+  }
+
+  // associate projects for ClinicalTrials
+  let keys = Object.keys(clinicalTrials);
+  for (let i = 0; i < keys.length; i++) {
+    if (clinicalTrials[keys[i]].publications) {
+      clinicalTrials[keys[i]].publications.forEach(pub => {
+        publications[pub].projects.forEach(proj => {
+          if (!clinicalTrials[keys[i]].projects) {
+            clinicalTrials[keys[i]].projects = [];
+          }
+          clinicalTrials[keys[i]].projects.push(proj);  // there may be duplicates if multiple publications come from the same project
+        });
+      });
     }
   }
 
@@ -60,6 +97,11 @@ const generateDataModel = async () => {
       projects[projectID][key] = projects[projectID][key] ? projects[projectID][key] : "";  // ensure all values are at least empty string
     });
     projects[projectID].abstract_text = projects[projectID].abstract_text ? projects[projectID].abstract_text.replace(/\n/g, "\\n") : "";  // format the abstract
+    // Publications key off of this project_id for relationships in Neo4j upon data loading. When publications are scraped, they are scraped with respect to
+    //  the activiity code and core id -- we don't have information more granular than that -- an entire set of projects that share a (activity code + core id) are
+    //  all treated the same. Therefore, this 'project_id' property must be formatted into just the activity code + core id. The true project ids (with suffix and leading numeral)
+    //  are preserved in the 'projects' data structure as 'given_project_id'.
+    projects[projectID].project_id = getActivityCode(projects[projectID].project_id) + getCoreId(projects[projectID].project_id);
   }
 
   // format publications fields for writing to file
@@ -77,6 +119,8 @@ const generateDataModel = async () => {
       geos[geoID][key] = geos[geoID][key] ? geos[geoID][key] : "";  // ensure all values are at least empty string
     });
     geos[geoID].title = geos[geoID].title ? geos[geoID].title.replace(/(\r\n|\r|\n|\t)/gm, "") : "";  // format the title
+    geos[geoID].publications = [...new Set(geos[geoID].publications)];  // unique publications
+    geos[geoID].projects = [...new Set(geos[geoID].projects)];  // unique projects
   }
 
   // format SRA fields for writing to file
@@ -86,6 +130,8 @@ const generateDataModel = async () => {
       sras[sraID][key] = sras[sraID][key] ? sras[sraID][key] : "";  // ensure all values are at least empty string
     });
     sras[sraID].study_title = sras[sraID].study_title ? sras[sraID].study_title.replace(/(\r\n|\r|\n|\t)/gm, "") : "";  // format the study_title
+    sras[sraID].publications = [...new Set(sras[sraID].publications)];  // unique publications
+    sras[sraID].projects = [...new Set(sras[sraID].projects)];  // unique projects
   }
 
   // format dbGaP fields for writing to file
@@ -95,11 +141,25 @@ const generateDataModel = async () => {
       dbgaps[dbgapID][key] = dbgaps[dbgapID][key] ? dbgaps[dbgapID][key] : "";  // ensure all values are at least empty string
     });
     dbgaps[dbgapID].title = dbgaps[dbgapID].title ? dbgaps[dbgapID].title.replace(/(\r\n|\r|\n|\t)/gm, "") : "";  // format the title
+    dbgaps[dbgapID].publications = [...new Set(dbgaps[dbgapID].publications)];  // unique publications
+    dbgaps[dbgapID].projects = [...new Set(dbgaps[dbgapID].projects)];  // unique projects
+  }
+
+  // format clinicalTrial fields for writing to file
+  for (let clinicalTrialID in clinicalTrials) {
+    let keys = Object.keys(clinicalTrials[clinicalTrialID]);
+    keys.forEach(key => {
+      clinicalTrials[clinicalTrialID][key] = clinicalTrials[clinicalTrialID][key] ? clinicalTrials[clinicalTrialID][key] : "";  // ensure all values are at least empty string
+    });
+    if (clinicalTrials[clinicalTrialID].publications) {
+      clinicalTrials[clinicalTrialID].publications = [...new Set(clinicalTrials[clinicalTrialID].publications)];  // unique publications
+    }
+    clinicalTrials[clinicalTrialID].projects = [...new Set(clinicalTrials[clinicalTrialID].projects)];  // unique projects
   }
 };
 
 // the column names and the data structure field names need to be the same
-const writeToFile = (filepath, columns, dataStructure, type) => {
+const writeToDataFile = (filepath, columns, dataStructure, type) => {
   let data = "";
   data += columns.join("\t") + "\n";
   for (let key in dataStructure) {
@@ -110,22 +170,57 @@ const writeToFile = (filepath, columns, dataStructure, type) => {
         temp.push(dataStructure[key][col]);
       }
     });
-    // for the passed columns, if there are projects or publications, they should be listed last. project before publication or just project at the end if no publications
-    // if a data structure does not inherently have publications or projects, but that information should be listed, the generateDataModel function is where those
+    // For the passed columns, projects.project_id or program.program_id should be listed last (whichever applies).
+    // All entities should point to only a project or a program, all other information/relationships should be queried, not raw.
+    // If a data structure does not inherently have projects (or all of the projects, some via publication), the generateDataModel function is where those
+    //   relationships should be populated/filled out. the note about projects in the columns passed still applies
+    if (dataStructure[key].projects) {
+      dataStructure[key].projects.map((p) => {
+        data += temp.join("\t") + "\t" + p + "\n";
+      });
+    }
+    else if (dataStructure[key].program) {  // if it doesn't have projects, it is a project, which has a program
+      data += temp.join("\t") + "\t" + dataStructure[key].program + "\n";
+    }
+    else {
+      data += temp.join("\t") + "\n";
+    }
+  }
+  fs.writeFileSync(filepath, data);
+};
+
+// the column names and the data structure field names need to be the same
+// explicitly writes publication relationships
+const writeToDataDigestFile = (filepath, columns, dataStructure, type) => {
+  let data = "";
+  data += columns.join("\t") + "\n";
+  for (let key in dataStructure) {
+    let temp = [type, key];  // the first column is 'type' and is hard coded and the same for the entire file
+                             //  the second column is the key for the data structure
+    columns.forEach(col => {  // iterate through the column properties in order
+      if (dataStructure[key][col] != undefined) {  // some columns don't represent data structure properties
+        temp.push(dataStructure[key][col]);
+      }
+    });
+    // for the passed columns, if there are projects or publications or programs, they should be listed last. program before project before publication
+    // if a data structure does not inherently have a program or publications or projects, but that information should be listed, the generateDataModel function is where those
     //   relationships should be populated. the note about projects and publications in the columns passed still applies
     if (dataStructure[key].publications || dataStructure[key].projects) {
-      if (dataStructure[key].publications) {  // if there are associated publications, preserve the upstream project id before the publication id
+      if (dataStructure[key].publications) {  // if there are associated publications, preserving upstream project and program
         dataStructure[key].publications.map((p) => {
           publications[p].projects.map((pp) => {
             data += temp.join("\t") + "\t" + pp + "\t" + p + "\n";
           });
         });
       }
-      if (dataStructure[key].projects) {  // if there are associated projects
+      if (dataStructure[key].projects) {  // if there are associated projects, preserving upstream program
         dataStructure[key].projects.map((p) => {
           data += temp.join("\t") + "\t" + p + "\n";
         });
       }
+    }
+    else if (dataStructure[key].program) {  // it is assumed a dataStructure entity has only one program associated with it, if any explicitly
+      data += temp.join("\t") + "\t" + dataStructure[key].program + "\n";
     }
     else {
       data += temp.join("\t") + "\n";
@@ -350,42 +445,61 @@ const run = async (projectsTodo) => {
   console.log('Writing Files...');
   // project file
   // writeToProjectFile();
-  let columns = ["type", "given_project_id", "project_id","application_id", "fiscal_year", "project_title", "abstract_text", "keywords",
-  "org_name", "org_city", "org_state", "org_country", "principal_investigators", "program_officers", "award_amount",
-   "nci_funded_amount", "award_notice_date", "project_start_date", "project_end_date", "full_foa"];
-  let filepath = 'data/project.tsv';
-  writeToFile(filepath, columns, projects, "project");
+  let columns = ["type", "given_project_id", "project_id","application_id", "fiscal_year", "project_title", "project_type", "abstract_text", "keywords",
+   "org_name", "org_city", "org_state", "org_country", "principal_investigators", "lead_doc", "program_officers", "award_amount",
+    "nci_funded_amount", "award_notice_date", "project_start_date", "project_end_date", "full_foa", "program.program_id"];
+  let filepath = 'digest_data/project.tsv';
+  writeToDataDigestFile(filepath, columns, projects, "project");
+  filepath = 'data/project.tsv';
+  writeToDataFile(filepath, columns, projects, "project");
 
   // publications file
   // writeToPublicationFile();
   columns = ["type","publication_id", "year", "journal", "title", "authors",
    "publish_date", "citation_count", "relative_citation_ratio", "rcr_range", "nih_percentile", "doi", "project.project_id"];
+  filepath = 'digest_data/publication.tsv';
+  writeToDataDigestFile(filepath, columns, publications, "publication");
+  columns = ["type","publication_id", "year", "journal", "title", "authors",
+  "publish_date", "citation_count", "relative_citation_ratio", "rcr_range", "nih_percentile", "doi", "project.project_id"];
   filepath = 'data/publication.tsv';
-  writeToFile(filepath, columns, publications, "publication");
+  writeToDataFile(filepath, columns, publications, "publication");
 
   // GEO file
   // writeToGEOFile();
   columns = ["type","accession","title", "status", "submission_date","last_update_date", "project.project_id", "publication.publication_id"];
+  filepath = 'digest_data/geo.tsv';
+  writeToDataDigestFile(filepath, columns, geos, "geo");
+  columns = ["type","accession","title", "status", "submission_date","last_update_date", "project.project_id"];
   filepath = 'data/geo.tsv';
-  writeToFile(filepath, columns, geos, "geo");
+  writeToDataFile(filepath, columns, geos, "geo");
 
   // SRA file
   // writeToSRAFile();
-  columns = ["type","accession","study_title", "bioproject_accession", "registration_date","project.project_id", "publication.publication_id"];
+  columns = ["type","accession","study_title", "bioproject_accession", "registration_date", "project.project_id", "publication.publication_id"];
+  filepath = 'digest_data/sra.tsv';
+  writeToDataDigestFile(filepath, columns, sras, "sra");
+  columns = ["type","accession","study_title", "bioproject_accession", "registration_date", "project.project_id"];
   filepath = 'data/sra.tsv';
-  writeToFile(filepath, columns, sras, "sra");
+  writeToDataFile(filepath, columns, sras, "sra");
 
   // dbGaP file
   // writeToDBGapFile();
   columns = ["type", "accession", "title", "release_date", "project.project_id", "publication.publication_id"];
+  filepath = 'digest_data/dbgap.tsv';
+  writeToDataDigestFile(filepath, columns, dbgaps, "dbgap");
+  columns = ["type", "accession", "title", "release_date", "project.project_id"];
   filepath = 'data/dbgap.tsv';
-  writeToFile(filepath, columns, dbgaps, "dbgap");
+  writeToDataFile(filepath, columns, dbgaps, "dbgap");
+
 
   // clinical trials file
   // writeToClinicalTrialsFile();
   columns = ["type", "clinical_trial_id", "title", "last_update_posted", "recruitment_status", "project.project_id", "publication.publication_id"];
+  filepath = 'digest_data/clinical_trial.tsv';
+  writeToDataDigestFile(filepath, columns, clinicalTrials, "clinical_trial");
+  columns = ["type", "clinical_trial_id", "title", "last_update_posted", "recruitment_status", "project.project_id"];
   filepath = 'data/clinical_trial.tsv';
-  writeToFile(filepath, columns, clinicalTrials, "clinical_trial");
+  writeToDataFile(filepath, columns, clinicalTrials, "clinical_trial");
 
   // writeToMetricsFile();
 };
