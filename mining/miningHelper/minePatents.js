26d2b4d690ea17ed961f37dbce4b97ecb23f3ec5
Alexander DeForge <alexanderdeforge@gmail.com>
2022-02-02 11:44:29 -0500

    Patent scraping functionality.

--- mining/miningHelper/minePatents.js
+++ mining/miningHelper/minePatents.js
@@ -1,50 +1,78 @@
 const {
   fetch,
   getCoreId,
-  fetchWithStatusCheck,
-  getActivityCode,
+  getActivityCode
 } = require('../../common/utils');
 const apis = require('../../common/apis');
-const {
-  loadCache,
-  writeToCache
-} = require('../miningHelper/miningCacher');
 
 
-const searchPatents = async (search_terms, patents, project) => {
-    search_terms.forEach(term => {
+// we can't use caching because projects update dynamically
+const searchPatents = async (search_terms, patents, project_group) => {
+    let counter = 0;
+    for (let i = 0; i < search_terms.length; i++) {
+        let term = search_terms[i];
+        console.log(apis.usptoPatentsSite + term);
         let d = await fetch(apis.usptoPatentsSite + term, true);  // true is keep trying
-        let temp = d;
-        let idx = temp.indexOf("#:~:text=" + term + "\">");  // 11 characters plus length of 'term'
-        while (idx > -1) {
-            temp = temp.substring(idx + 11 + term.length);
-            idx = temp.indexOf("</a></td>");  // 9 characters
-            if (idx > -1) {
-                let p = temp.substring(idx);
+        let pattern = new RegExp(/\/patent\/app\/\d{11}#:~:text=/, "g");  // patent numbers are 11 digits, 01/31/2022 adeforge are they always 11 digits?
+        let result = d.match(pattern);
+        result = [...new Set(result)];  // unique results
+        if (result !== null) {
+            for (let j = 0; j < result.length; j++) {
+                result[j] = result[j].substring(12);  // get past '/patent/app/'
+                let idx = result[j].indexOf("#:~:text=");
+                let p = result[j].substring(0,idx);  // the patent id we want
                 if (!patents[p]) {
                     patents[p] = {};
+                    counter += 1;
+                }
+                if (!patents[p].queried_project_id) {
+                    patents[p].queried_project_id = [];
+                }
+                if (patents[p].queried_project_id.indexOf(term) === -1) {
+                    patents[p].queried_project_id.push(term);
                 }
                 if (!patents[p].projects) {
                     patents[p].projects = [];
                 }
-                patents[p].projects.push(project);
-                temp = temp.substring(idx + 9);
+                project_group.forEach(element => {
+                    if (patents[p].projects.indexOf(element) === -1) {
+                        patents[p].projects.push(element);
+                    }
+                });
             }
-            idx = temp.indexOf("#:~:text=" + term);
         }
-    });
+    }
+    console.log("Found " + counter + " patent(s).");
+};
+
+const prepareSearchTerms = (projects) => {
+    let cluster = {};
+    const keys = Object.keys(projects);
+    for (let i = 0; i < keys.length; i++) {
+        let project_core_id = getCoreId(keys[i]);
+        let project_activity_code = getActivityCode(keys[i]);
+
+        if (cluster[project_activity_code + project_core_id] == undefined) {  // if we haven't seen this activity code + core id yet, make a new entry
+            cluster[project_activity_code + project_core_id] = {"search_terms": [project_activity_code + project_core_id, project_core_id], "project_group": [keys[i]]};
+        }
+        else {  // if we have seen it, append a project
+            if (cluster[project_activity_code + project_core_id].project_group.indexOf(keys[i]) === -1) {
+                cluster[project_activity_code + project_core_id].project_group.push(keys[i]);
+            }
+        }
+    }
+    return cluster;
 };
 
 const run = async (projects, patents) => {
-  const projectNums = Object.keys(projects);
-  for (let i = 0; i < projectNums.length; i++) {
-      let project_core_id = getCoreId(projectNums[i]);
-      let project_activity_code = getActivityCode(projectNums[i]);
-
-      let search_terms = [project_activity_code + project_core_id, project_core_id];
-
-      await searchPatents(search_terms, patents, projectNums[i]);
-  } 
+    let cluster = prepareSearchTerms(projects);
+    const keys = Object.keys(cluster);
+    for (let i = 0; i < keys.length; i++) {
+        let search_terms = cluster[keys[i]].search_terms;
+        let project_group = cluster[keys[i]].project_group;
+        console.log("Mining patents for project group: " + keys[i] + " [" + (i+1) + "/" + keys.length + "]");  // the first search term is activity code + core id
+        await searchPatents(search_terms, patents, project_group);
+    } 
 };
 
 module.exports = {
--- mining/miningHelper/minePatentsDetail.js
+++ mining/miningHelper/minePatentsDetail.js
@@ -1,37 +1,86 @@
 const {
   fetch,
-  getCoreId,
-  fetchWithStatusCheck,
-  getActivityCode,
+  fetchWithStatusCheck
 } = require('../../common/utils');
 const apis = require('../../common/apis');
-const {
-  loadCache,
-  writeToCache
-} = require('../miningHelper/miningCacher');
+// const {
+//   loadCache,
+//   writeToCache
+// } = require('../miningHelper/miningCacher');
 
 
 const searchPatentDetail = async (patent, patentID) => {
-    let d = await fetch(apis.usptoPatentsSite + patentID, true)  // true is keep trying
-    let temp = d;
-    let idx = temp.indexOf("US " + patentID + " ");  // 4 characters plus length of 'patentID' (the most reliable way to get this is at the bottom of the page, by inspection)
-    if (idx > -1) {
-        // get kind code
-        temp = temp.substring(idx + 4 + patentID.length);
-        idx = temp.indexOf("</div><hr>");
-        if (idx > -1) {
-            let kind_code = temp.substring(0, idx);
-            patent.kind_code = kind_code;
-        }
+    console.log(apis.usptoPatentsDetailSite + patentID);
+    let d = await fetch(apis.usptoPatentsDetailSite + patentID, true);  // true is keep trying
+    if (d !== "failed") {
+        if (d.indexOf("<title>  []</title>") === -1) {
+            let temp = d;
+            // get kind code
+            let idx = temp.indexOf("<b>Kind Code</b></td>");  // 21 characters
+            if (idx > -1) {
+                temp = temp.substring(idx + 21);
+                idx = temp.indexOf("</b></td>");
+                let kind_code = temp.substring((idx-2), idx);
+                patent.kind_code = kind_code;
+            }
+            else {
+                patent.kind_code = "N/A";
+            }
 
-        // get filed date
-        temp = d;
-        idx = temp.indexOf("<tr><td>Filed Date</td><td>");  // 27 characters
-        if (idx > -1) {
-            temp = temp.substring(idx + 27);
-            idx = temp.indexOf("</td></tr></table>");
-            let filed_date = temp.substring(0, idx);
-            patent.filed_date = filed_date;
+            // get filed date
+            temp = d;
+            idx = temp.indexOf("<tr><td>Filed Date</td><td>");  // 27 characters
+            if (idx > -1) {
+                temp = temp.substring(idx + 27);
+                idx = temp.indexOf("</td></tr></table>");
+                let filed_date = temp.substring(0, idx);
+                patent.filed_date = filed_date;
+            }
+            else {
+                patent.filed_date = "N/A";
+            }
+
+            // get cited project ids
+            patent.cited_pattern_project_id = "N/A";
+            patent.cited_raw_project_id = "N/A";
+            temp = d;
+            idx = temp.indexOf("Goverment Interests");  // 19 characters, it is misspelled on the site
+            if (idx > -1) {
+                temp = temp.substring(idx);
+                idx = temp.indexOf("Claims");
+                if (idx > -1) {
+                    temp = temp.substring(0,idx);  // what we want to search is in the 'Goverment[sic] Interests' section before the 'Claims' section
+                    for (let i = 0; i < patent.queried_project_id.length; i++) {
+                        // pattern means find what's in this section of the forms we're aware of
+                        if (patent.cited_pattern_project_id === "N/A") {
+                            patent.cited_pattern_project_id = [];
+                        }
+                        let pattern = /([A-Z][0-9]{2}[ -]?)?[A-Z]{2}[0-9]{6}([ -]?[0-9]{2}([A-Z][0-9])?)?/g;  // matches a project ID
+                        let t = temp.replace(/(\r\n|\r|\n|\t)/gm, "");
+                        t = t.match(pattern);
+                        if (t) {
+                            patent.cited_pattern_project_id.push(...t);  // strip newlines (and tabs) and pattern match
+                        }
+
+                        // raw means see what's around the found project id
+                        idx = temp.indexOf(patent.queried_project_id[i]);
+                        while (idx > -1) {
+                            if (patent.cited_raw_project_id === "N/A") {
+                                patent.cited_raw_project_id = [];
+                            }
+                            let low_end = idx - 15;
+                            low_end = (idx < 0) ? 0 : low_end;
+                            let high_end = idx + patent.queried_project_id[i].length + 15;
+                            high_end = (idx > temp.length) ? temp.length-1 : high_end;
+                            patent.cited_raw_project_id.push(temp.substring(low_end,high_end).replace(/(\r\n|\r|\n|\t)/gm, ""));  // 15 characters on either side of the matched query term
+                            temp = temp.substring(idx + patent.queried_project_id[i].length);  // get past the queried term
+                            idx = temp.indexOf(patent.queried_project_id[i]);  // see if there is another result
+                        }
+                    }
+                    console.log("Citation details acquired.");
+                }
+            }
+            console.log("Details acquired for patent " + patentID);
         }
     }
 };
@@ -39,6 +88,7 @@ const searchPatentDetail = async (patent, patentID) => {
 const run = async (patents) => {
     const patentNums = Object.keys(patents);
     for (let i = 0; i < patentNums.length; i++) {
+        console.log("Mining details for patent: " + patentNums[i] + " [" + (i+1) + "/" + patentNums.length + "]");
         await searchPatentDetail(patents[patentNums[i]], patentNums[i]);
     }
 };
