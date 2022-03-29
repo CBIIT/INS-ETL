dde91bc62893d61258f87b91c65a019d8cc03fe8
Alexander DeForge <alexanderdeforge@gmail.com>
2022-03-29 12:58:30 -0400

    Updated source for project id's.

--- index.js
+++ index.js
@@ -12,19 +12,18 @@ console.log(`Received paramaters: ${args}`);
 
 console.log("Start processing...");
 
-let file_path = path.join(__dirname, 'config', 'moonshot_ccdi_projects_all.xlsx');
+let file_path = path.join(__dirname, 'config', 'Moonshot IDs_no P30_031622_suff&pref_v2.xlsx');
 
 readXlsxFile(file_path, { sheet: 1 }).then(async (rows) => {
   console.time('full_run');
   let projects = {};
     
     rows.forEach((item, idx) => {
-        if (idx >= 667) {  // 11/03/2021 adeforge, see 'moonshot_ccdi_projects_all.xlsx', projects sheet in the config directory
+        if (idx >= 1) {  // skip the header
           projects[item[0]] = {};
-          // 12/28/2021 adeforge, not all of these are present for the current 'moonshot_ccdi_projects_all.xlsx' project ids
           projects[item[0]].project_type = item[1];
           projects[item[0]].program = item[3];
-          projects[item[0]].lead_doc = "N/A";
+          projects[item[0]].lead_doc = item[4];
         }
     });
 
