6940c61540cf73abdf98ff4442e11f28e72ab580
Alexander DeForge <alexanderdeforge@gmail.com>
2023-03-02 08:51:56 -0500

    Progress on Python script, pausing to work on core-project-effort.

--- date_restriction_for_outputs.py
+++ date_restriction_for_outputs.py
@@ -72,4 +72,20 @@ for file in dbgap_files:
 # we will care to compare the award_notice_date through the publication link to all outputs and their relevant dates
 ###############################################
 
+# populate main dataframe relating publication_id to award_notice_date of interest (and also keep queried_project_ids)
+#   clinical trials by core project will be handled later
+df_lookup = None
 
+# prepare the publication dataframe for merger
+df_publication = df_publication.rename(columns={"project.queried_project_id": "queried_project_id"})
+df_publication = df_publication[["publication_id","queried_project_id"]]
+
+# prepare the project dataframe for merger
+df_project["award_notice_date"] = df_project["award_notice_date"].apply(lambda x: datetime.strptime(x, '%d-%b-%Y'))  # format as datetime from string time
+df_project = df_project.groupby("queried_project_id", group_keys=False).apply(lambda x: pd.DataFrame({"queried_project_id": x["queried_project_id"], "award_notice_date": min(x["award_notice_date"])}))
+
+# publications should be unique and be associated with the oldest award_notice_date (and associated queried_project_id) from its upstream grants
+df_lookup = df_publication.merge(df_project, on="queried_project_id").drop_duplicates().reset_index(drop=True)
+df_lookup = df_lookup.groupby("publication_id", as_index=False, group_keys=False).apply(lambda x: pd.DataFrame([{"award_notice_date": min(x["award_notice_date"]), "publication_id": x.iloc[0]["publication_id"], "queried_project_id": x.sort_values("award_notice_date").reset_index(drop=True).iloc[0]["queried_project_id"]}])).reset_index(drop=True)
+print(len(df_lookup))
+print(df_lookup.head(15))
